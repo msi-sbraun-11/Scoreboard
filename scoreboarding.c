648f633dea7ae923a6df5878b4ed54e2132fab02
M Sai Subramaniam <147979737+msi-sbraun-11@users.noreply.github.com>
2023-10-16 20:22:40 +0530

    Add files via upload

--- scoreboarding.c
+++ scoreboarding.c
@@ -1,11 +1,11 @@
-// simple scoreboard
+// simple scoreboard for 6 instructions
 #include <stdbool.h>
-#define ADD1 1
-#define ADD2 2
-#define MUL1 3
-#define MUL2 4
-#define DIV 5
-#define INTUNIT 6
+#define ADD1 0
+#define ADD2 1
+#define MUL1 2
+#define MUL2 3
+#define DIV 4
+#define INTUNIT 5
 
 typedef struct record
 {
@@ -19,14 +19,14 @@ typedef struct record
 int R[32];      // integer register file
 float F[16];    // FP register file
 
-short int FPAdder[2][2], // 2 add units; takes 2 cycles for EX
-    FPMul[2][2],   // 1 multiply units; takes 6 cycles for EX
-    FPDiv[1][2],   // 1 division unit; takes 11 cycles for EX
-    IntALU[1][2];  // 1 integer unit; takes 1 cycle for EX
+short int FPAdder[2][2], // 2 add units; s, t; takes 2 cycles for EX
+    FPMul[2][2],   // 1 multiply units; s, t; takes 6 cycles for EX
+    FPDiv[1][2],   // 1 division unit; s, t; takes 11 cycles for EX
+    IntALU[1][2];  // 1 integer unit; s, t; takes 1 cycle for EX
 
 /** components of the Scoreboard **/
-short int InstStatus[4][4];
-record Scoreboard[6];
+short int InstStatus[6][4]; // for 6 instructions only
+record Scoreboard[6]; // functional units
 int RegStatus[16];
 
 void issue();
@@ -36,6 +36,16 @@ void writeResult();
 
 int main()
 {
+    short int cycle = 0, inst_completed = 0;
+    while(inst_completed != 6)
+    {
+
+    }
 
     return 0;
+}
+
+void issue()
+{
+
 }
