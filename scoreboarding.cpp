8d35df81b82857f4b4a711f2964071490ab3aad7
M Sai Subramaniam <147979737+msi-sbraun-11@users.noreply.github.com>
2023-10-20 15:24:20 +0530

    Add files via upload

--- scoreboarding.cpp
+++ scoreboarding.cpp
@@ -26,13 +26,21 @@ using namespace std;
 
 class record
 {
+    enum{ADD, MUL, DIV, ADDER} FuncUnitType;
+    enum {add, mul, div, load, store};
     bool busy;            // is the functional unit being used?
     short int op;         // opcode
     short int Fi, Fj, Fk; // destination, source1, source2
     short int Qj, Qk;     // functional units producing Fj and Fk
     bool Rj, Rk;          // is Fj and Fk available?
+    bool immediate;       // is the instruction an immediate instruction? (needed for LOAD and STORE operations)
     short int StallsLeft; // need to count how many stalls left; helps in indicating that the instruction has executed
     short int InstStatus; // useful to know at which clock cycle instruction finished its stage
+    union
+    {
+        float ValF;
+        int ValI;
+    }Vj, Vk;
 
     public:
         record()
@@ -44,134 +52,146 @@ class record
             StallsLeft = 0;
             InstStatus = 0;
         }
+        friend class scoreboard;    
 };
 
 class scoreboard
 {
     int cycle;
     vector<record> fu_status(NUM_FU);     // functional unit status
-    vector<int> R(INTREGFILESIZE);      // Integer register file
-    vector<float> F(FLOATREGFILESIZE);  // FP register file
+    vector<int> R(INTREGFILESIZE);      // Integer register file FLOATREGFILESIZE...FLOATREGFILESIZE+INTREGFILESIZE-1
+    vector<float> F(FLOATREGFILESIZE);  // FP register file 0...FLOATREGFILESIZE-1
     vector<int> FloatRegStatus(-1, FLOATREGFILESIZE), IntRegStatus(-1, INTREGFILESIZE); // register status
 
     // we are giving register number, not the value as such; so let's make it <int,int>.
     // In the readOperands() and execute() function, we can get the value and pass.
 
     /* vector<pair<float, float>> FPAdder(NUM_FPADD), // 2 add units; s, t;          takes 2 cycles for EX
-            FPMul(NUM_FPMUL),                      // 2 multiply units; s, t;     takes 6 cycles for EX
-            FPDiv(NUM_FPDIV);                      // 1 division unit; s, t;      takes 11 cycles for E
-    vector<pair<int, int>> Adder(NUM_ADDER);       // 1 adder for calculating 
-                                                   // effective address; s, t;    takes 1 cycle for EX */
+            FPMul(NUM_FPMUL),                         // 2 multiply units; s, t;     takes 6 cycles for EX
+            FPDiv(NUM_FPDIV);                         // 1 division unit; s, t;      takes 11 cycles for E
+    vector<pair<int, int>> Adder(NUM_ADDER);          // 1 adder for calculating 
+                                                      // effective address; s, t;    takes 1 cycle for EX */
 
     void setrecord(int, int, int, int, int);
     public:
-        bool issue(string, int, int, int);
+        bool issue(string, int, int, int, bool);
         bool readOperands(int);
         bool execute();
         bool writeResult();
 };
 
-void setrecord(int fu, int opcode, int d, int s, int t)
+void scoreboard::setrecord(int fu, int opcode, int d, int s, int t, bool imm)
 {
-    fu_status[fu].busy = true;
-    fu_status[fu].op = opcode;
-    fu_status[fu].Fi = d;
-    fu_status[fu].Fj = s;
-    fu_status[fu].Fk = t;
-    FloatRegStatus[fu_status[fu].Fi] = fu;
-    fu_status[fu].Qj = FloatRegStatus[Fj];
-    fu_status[fu].Qk = FloatRegStatus[Fk];
-    if(fu_status[fu].Qj == -1)
-        fu_status[fu].Rj = true;
-    else fu_status[fu].Rj = false;
-    if(fu_status[fu].Qk == -1)
-        fu_status[fu].Rk = true;
-    else fu_status[fu].Rk = false;
+    auto rec = fu_status[fu];
+    rec.busy = true;
+    rec.op = opcode;
+    switch(rec.op)
+    {
+        case add: rec.FuncUnitType = ADD;
+                  break;
+        case mul: rec.FuncUnitType = MUL;
+                  break;
+        case div: rec.FuncUnitType = DIV;
+                  break;
+        case load:
+        case store: rec.FuncUnitType = ADDER;
+                    break;
+    }
+    rec.immediate = imm;
+    rec.Fi = d;
+    rec.Fj = s;
+    rec.Fk = t;
+    FloatRegStatus[rec.Fi] = fu;
+    rec.Qj = FloatRegStatus[Fj];
+    rec.Qk = FloatRegStatus[Fk];
+    if(rec.Qj == -1)
+        rec.Rj = true;
+    else rec.Rj = false;
+    if(rec.Qk == -1)
+        rec.Rk = true;
+    else rec.Rk = false;
 }
 
-bool scoreboard::issue(string op, int d, int s, int t) // need to take care of offset shift to diff bw IntRegFile and FloatRegFile
+bool scoreboard::issue(string op, int d, int s, int t, bool imm) // need to take care of offset shift to diff bw IntRegFile and FloatRegFile
 {
+    // add 0 mul 1 div 2 load 3 store 4
     unordered_map<string, int> optab = {
-        {"L.D", 0},
-        {"S.D", 1},
-        {"MUL.D", 2},
-        {"ADD.D", 3},
-        {"DIV.D", 4},
-         };
+        /* {"L.D", 0},
+        {"S.D", 0},
+        {"MUL.D", 1},
+        {"ADD.D", 2},
+        {"DIV.D", 3}, */
+        {"ADD.D", add},
+        {"MUL.D", mul},
+        {"DIV.D", div},
+        {"L.D", load},
+        {"S.D", store}
+    };
     bool issued = false; // We need to indicate that the instruction has been issued so that in the execution loop, 
                          // we can move to the next stage
+
     switch(optab[op])
     {
-        case 0: 
+        case load: 
                 if(!fu_status[ADDER].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(ADDER, optab[op], d, s, t);
+                    setrecord(ADDER, optab[op], d, s, t, imm);
                     issued = true;
                 }    
-                else
-                    issued = false;
                 break;
 
-        case 1:
+        case store:
                 if(!fu_status[ADDER].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(ADDER, optab[op], d, s, t);
+                    setrecord(ADDER, optab[op], d, s, t, imm);
                     issued = true;
                 }    
-                else
-                    issued = false;
                 break;
 
-        case 2:
+        case mul:
                 if(!fu_status[MUL1].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(MUL1, optab[op], d, s, t);
+                    setrecord(MUL1, optab[op], d, s, t, imm);
                     issued = true;
                 }    
                 else if(!fu_status[MUL2].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(MUL2, optab[op], d, s, t);
+                    setrecord(MUL2, optab[op], d, s, t, imm);
                     issued = true;
                 }
-                else
-                    issued = false;
                 break;
 
-        case 3:
+        case add:
                 if(!fu_status[ADD1].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(ADD1, optab[op], d, s, t);
+                    setrecord(ADD1, optab[op], d, s, t, imm);
                     issued = true;
                 }
                 else if(!fu_status[ADD2].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(ADD2, optab[op], d, s, t);
+                    setrecord(ADD2, optab[op], d, s, t, imm);
                     issued = true;
                 }
-                else
-                    issued = false;
                 break;
 
-        case 4:
+        case div:
                 if(!fu_status[DIV1].busy &&
                     find(FloatRegStatus.begin(), FloatRegStatus.end(), d) == FloatRegStatus.end())
                 {
-                    setrecord(DIV1, optab[op], d, s, t);
+                    setrecord(DIV1, optab[op], d, s, t, imm);
                     issued = true;
                 }
-                else
-                    issued = false;
                 break;
     }
 
     return issued;
-/*     if(strcmp(op, "L.D") == 0)
+/*  if(strcmp(op, "L.D") == 0)
     {
         if(Scoreboard[ADDER].busy == false)
         {
@@ -211,21 +231,32 @@ bool scoreboard::issue(string op, int d, int s, int t) // need to take care of o
     } */
 }
 
-bool readOperands(int fu) // doesn't need any arguments because all the data that it needs is present in the status table
+bool scoreboard::readOperands(int fu) // doesn't need any more arguments because all the data that it needs is present in the status table
 {
-    if(fu_status[fu].Rj && fu_status[fu].Rk)
+    auto rec = fu_status[fu];
+    if(rec.Rj && rec.Rk)
     {
-        fu_status[fu].Fj = FloatRegStatus[fu_status[fu].Fj];
-        fu_status[fu].Fk = FloatRegStatus[fu_status[fu].Fk];
+        // If immediate, load the value itself
+        if(rec.immediate)
+            rec.Vj.ValI = rec.Fj;
+        else
+            rec.Vj.ValF = F[rec.Fj];
+        // I am addressing from where to load here itself; 
+        // so the convention that we are maintaining is 0...15-FPRegister file and 16...47-IntRegisterFile
+        if(rec.Fk < 16)
+            rec.Vk.ValF = F[rec.Fk];
+        else
+            rec.Vk.ValI = R[rec.Fk-16];
         return true;
     }    
     else
         return false;
 }
 
-bool execute()
+bool scoreboard::execute(int fu)
 {
-
+    auto temp;
+    
 }
 
 bool writeResult()
